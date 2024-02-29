19cf4d2ed22b7f9da21a1438a1e49f06e9c00183
0xNunana <kudayapaul@gmail.com>
2024-02-29 20:06:57 +0000

    text color

--- sections/Courses.tsx
+++ sections/Courses.tsx
@@ -8,7 +8,7 @@ const Courses = () => {
     <section className='  bg-slate-200 ' id='courses'>
         <div className='py-7'>
             <p className='text-center text-4xl md:text-5xl lg:text-6xl font-bold '>
-                Explore <span className='text-blue-500'>Courses</span>
+                Explore <span className='text-[#665ad2]'>Courses</span>
             </p>
         </div>
     <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-5 py-7 px-16 md:px-2 max-w-screen-2xl mx-auto'>
