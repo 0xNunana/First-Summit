e6ad4edff910478cc9a3c60d99660f57df868e7c
0xNunana <kudayapaul@gmail.com>
2024-03-01 17:01:13 +0000

    metadata changed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,24 @@
 import type { Metadata } from "next";
-import { Inter,Montserrat } from "next/font/google";
+import {Montserrat } from "next/font/google";
 import "./globals.css";
 import Header from "@/sections/Header";
 import Footer from "@/sections/Footer";
 
-const inter = Inter({ subsets: ["latin"] });
 const mont = Montserrat({subsets:['latin'],weight:['900','800','700','600','500','400','300']})
 
 export const metadata: Metadata = {
   title: "First Summit IT",
-  description: "Learn Data Science",
+  description: "Learn Database Administration,Data Science or AI in just 5 months",
+  keywords:'Data Science, Database Administration,AI,Learn',
+  category: 'technology,education, science',
+  // openGraph:{
+  //   type: "website",
+  //   title: "First Summit IT",
+  //   description: "Learn Database Administratio,Data Science or AI in just 5 months",
+  //   siteName: "First Summit IT",
+  
+  // }
+
 };
 
 export default function RootLayout({
